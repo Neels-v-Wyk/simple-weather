4da2b06977da0dd271b73a5328cec5217e0c1821
Neels-v-Wyk <git@neels.tech>
2021-06-12 22:11:23 +0200

    upgrade to https

--- src/page-creation/handleLocData.js
+++ src/page-creation/handleLocData.js
@@ -6,7 +6,7 @@ import createAtmosData from "./createAtmosData.js"
 
 export default async function handleLocData(location, content) {
 
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`);
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=1986480656ec490d950204923202611&q=${location}`);
     const responseJson = await response.json();
 
     createTempData(responseJson, content);
